f603931c6043273d9559e2fd0850f68ea0af38a6
Christian Nally <christian.nally@lighthouselabs.com>
2023-07-31 12:03:03 -0700

    further in-class edits

--- w02d1/README.md
+++ w02d1/README.md
@@ -4,12 +4,12 @@
 - [x] Manual testing
 - [x] Bringing in outside code
 - [x] Making assertions
-- [] Exporting our code
-- [] Intro to NPM: a Node package manager
-- [] Mocha test runner
-- [] Ignoring files/folders
-- [] What is TDD?
-- [] Chai assertion library
+- [x] Exporting our code
+- [x] Intro to NPM: a Node package manager
+- [x] Mocha test runner
+- [x] Ignoring files/folders
+- [x] What is TDD?
+- [x] Chai assertion library
 
 ### `module.exports` and `require`
 - We can export functions, objects, etc from a file using the `module.exports` object
--- w02d1/test/hello-world-test.js
+++ w02d1/test/hello-world-test.js
@@ -1,4 +1,5 @@
-const assert = require('assert'); // This line includes new definitions
+//const assert = require('assert'); // This line includes new definitions
+const assert = require('chai').assert; // This line includes new definitions
 const objectOfFunctions = require('../hello-world');
 
 describe('things to say at the beginning or end of a conversation', () => {
@@ -8,6 +9,11 @@ describe('things to say at the beginning or end of a conversation', () => {
         assert.equal(whatWasSaid, 'Hello, Monkey Fuzz!');
     });
     
+    it('asks how the person is doing properly', () => {
+        const whatWasSaid = objectOfFunctions.sayHowAreYou('Monkey Fuzz');
+        assert.equal(whatWasSaid, 'How Are You, Monkey Fuzz?');
+    });
+
     it('says Goodbye properly', () => {
         // test code
         const whatWasSaid = objectOfFunctions.sayGoodbye('Monkey Fuzz');
